b64b9ab3cf41514bd4275f95eee981e0ad59d24c
mmeus <maxiking156274@gmail.com>
2025-12-15 12:35:01 +0100

    sdsadasa

---
+++ zad4.py
@@ -0,0 +1,23 @@
+dane = [w.strip() for w in open('sygnaly.txt')]
+
+#zad4.1
+for i in range(39, len(dane), 40):
+    print(dane[i][9], end = '')
+#print(''.join([[w.strip() for w in open('sygnaly.txt')][i][9] for i in range(39, len([w.strip() for w in open('sygnaly.txt')]), 40)]))
+
+#zad4.2
+
+#1. Tablica częstości:
+max_s = ''
+max_ile_roznych = 0
+
+for s in dane:
+    TC = [0] * 256
+    for z in s:
+        kod = ord(z)
+        TC[kod] = 1
+    ile_roznych = sum(TC)
+    if ile_roznych > max_ile_roznych:
+        max_ile_roznych = ile_roznych
+        max_s = s
+print(max_s, max_ile_roznych)
